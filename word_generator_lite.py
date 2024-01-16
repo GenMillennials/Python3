8607ce545430fd0110847d06b29ea7ae8253e242
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-01-16 10:32:25 +0300

    Added the optimized code vesrsion

--- word_generator_lite.py
+++ word_generator_lite.py
@@ -1,4 +1,4 @@
-def prod(alph,rep): # Definition the function with two parameters: set of letters, and length of result
+""" def prod(alph,rep): # Definition the function with two parameters: set of letters, and length of result
     L=[]   # Empty list is created for storing results
     if rep==1:  # Terminal cases working out, when input in "rep" argument one letter only
         for i in alph:
@@ -10,4 +10,17 @@ def prod(alph,rep): # Definition the function with two parameters: set of letter
     return L # Returning L into def prod(alph,rep)
 
 for p in prod("abc",4): 
-    print(p)  # Output elements from the list L[] as a result
+    print(p)  # Output elements from the list L[] as a result """
+
+# The optimized code version
+
+def prod(alph,rep):
+    L=[]
+    if rep==1:
+        L=[i for i in alph]
+    else:
+        L=[a+p for a in alph for p in prod(alph,rep-1)]
+    return L
+
+for p in prod("abc",4):
+    print(p)
