18560671703624f9428ed056147e3b983dcafc11
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-01-19 21:33:47 +0300

    Modified max_square_matrice

--- max_square_matrice.py
+++ max_square_matrice.py
@@ -1,7 +1,7 @@
 # Search the lenght of the biggest square inside a matrice (two-dimensional list).
 # The square should be filled of nulls only.
 
-def square(M,y=0,x=0,l=-1,L=[]):  # Definiton a function consists of 4 parameters: Matrice, two coordinates and length of the square side
+def square(M,y=0,x=0,l=-1,L=[]):
     if l==-1:
         l=len(M)
         L=[]
