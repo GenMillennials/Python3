9ece821aaefd603c3f862cc1e1ae003856bf1bcb
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-01-25 19:20:23 +0300

    Flag on the ship has been modified

--- flag_on_the_ship.py
+++ flag_on_the_ship.py
@@ -15,3 +15,19 @@ if c > 0:
     print("Someone got sick")
 else:
     print("All passengers are healthy") """
+
+
+""" T = [int(el) for el in input().split()]
+c = sum(list(map(lambda t: t > 37, T)))
+if c > 0:
+    print("Someone got sick")
+else:
+    print("All passengers are healthy") """
+
+
+T = [int(el) for el in input().split()]
+c = len(list(filter(lambda t: t > 37, T)))
+if c > 0:
+    print("Someone got sick")
+else:
+    print("All passengers are healthy")
