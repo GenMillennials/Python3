989da07a6f87d00f212ac8ee275b599771639035
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-01-27 16:07:49 +0300

    Partial exponentation has been modified

--- partial_exponentiation.py
+++ partial_exponentiation.py
@@ -1,4 +1,4 @@
-def genpow(n):
+""" def genpow(n):
     return lambda a: a ** n
 
 square = genpow(2)
@@ -9,4 +9,27 @@ print(cube(a))
 n = int(input())
 userpow = genpow(n)
 print(userpow(a))
+print(genpow(n) (a)) """
+
+
+def fastpow(a, n):
+    if n == 0:
+        return 1
+    elif n % 2 == 0:
+        return fastpow(a * a, n // 2)
+    else:
+        return fastpow(a, n-1) * a
+
+def genpow(n):
+    return lambda a: fastpow(a, n)
+
+square = genpow(2)
+cube = genpow(3)
+a = float(input())
+print(square(a))
+print(cube(a))
+n = int(input())
+userpow = genpow(n)
+print(userpow(a))
 print(genpow(n) (a))
+    
