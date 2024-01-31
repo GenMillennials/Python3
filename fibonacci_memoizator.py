c519f69914c0b68c87e77fdc3f24e21ff795e45b
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-01-31 18:35:59 +0300

    Fibonacci memoizator has been created

---
+++ fibonacci_memoizator.py
@@ -0,0 +1,22 @@
+def memo(func):
+    L = []
+    def result(num):
+        nonlocal L
+        if num >= len(L):
+            L = L + [-1] * (num + 1)
+        if L[num] == -1:
+            L[num] = func(num)
+        return L[num]
+    return result
+
+
+def fibonacci(num):
+    if num <= 2:
+        return 1
+    else:
+        return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+memfibonacci = memo(fibonacci)
+num = int(input())
+print(memfibonacci(num))
