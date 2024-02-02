154adcfc98dcb430f655c9f5f1192ac4a82a9044
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-02-02 09:10:12 +0300

    Progression has been modified

--- progression.py
+++ progression.py
@@ -14,7 +14,7 @@ for i in range(n):
     print(sum(L)) """
 
 # Geometric progression generator
-def geometric(n, start, step):
+""" def geometric(n, start, step):
         yield start
         for i in range(n - 1):
                 start = start * step
@@ -24,4 +24,19 @@ def geometric(n, start, step):
 n = int(input())
 L = [i for i in geometric(n, 1, 2)]
 print(L)
-print(sum(L))
+print(sum(L)) """
+
+
+# Common progression generator
+def progression(n, start=0, step=1, f=lambda a, b: a + b):
+    yield start
+    for i in range(n - 1):
+        start = f(start, step)
+        yield start
+
+
+print(list(progression(6)))
+print(list(progression(6, 1)))
+print(list(progression(6, 1, 2)))
+print(list(progression(6, 1, 2, lambda a, b: a * b)))
+print(list(progression(6, 3, 2, lambda a, b: a ** b)))
