734bf26e7909b32850637756ab0c5072e7f216fc
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-02-03 14:32:47 +0300

    Class Point has been modified for calculating distance

--- class_point.py
+++ class_point.py
@@ -1,16 +1,19 @@
 class point:
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
     def input(self):
         s=[]
         s=input().split()
         self.x=float(s[0])
         self.y=float(s[1])
+        return self
+
 
 def distance(A,B):
     return ((A.x - B.x) ** 2 + (A.y - B.y) ** 2) ** (0.5)
 
 
-A = point()
-B = point()
-A.input()
-B.input()
+A = point().input()
+B = point().input()
 print(distance(A,B))
