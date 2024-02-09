64f4a50b9bfcf66e1de5b379a66bcea2dab9f75f
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-02-09 10:34:11 +0300

    Matrix construction has been modified

--- matrix_construction.py
+++ matrix_construction.py
@@ -4,3 +4,19 @@ class matrix:
         self.array=[]
         for i in range(n):
             self.array.append([0]*n)
+    def __getitem__(self, i):
+        return self.array[i]
+    def __setitem__(self,i,v):
+        self.array[i]=v
+    def input(self):
+        self.array=[[float(el) for el in input().split()] for i in range(self.n)]
+        return self
+    def print(self):
+        for row in self:
+            print(row)
+        return self
+
+
+M=matrix(2)
+M.input()
+M.print()
