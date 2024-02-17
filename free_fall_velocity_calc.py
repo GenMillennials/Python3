9ce3b06c13c4dfd4874c51c3fe967ae241fdef10
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-02-17 13:33:48 +0300

    Free fall velocity calculator has been modified

--- free_fall_velocity_calc.py
+++ free_fall_velocity_calc.py
@@ -1,7 +1,11 @@
-# The formula is v = (v_init**2 + 2 * a * d) ** 0.5
-# a = 9.8 meters per squared second
-v_init = float(input("Enter the initial velocity "))
-a = 9.8
+# The general formula is v = sqrt(v_init**2 + 2 * a * gravity)
+# v_init = 0
+# The general formula falling time is sqrt(2 * d / gravity)
+# gravity = 9.8 meters per squared second
+from math import sqrt
+gravity = 9.8
 d = float(input("Enter the falling distance "))
-v = round(((v_init ** 2) + (2 * a * d)) ** 0.5, 2)
+v = round(sqrt((2 * gravity * d)), 2)
+falling_time = round(sqrt(2 * d / gravity), 2)
 print("The free fall velocity is", v, "meters per second at the ground")
+print("The falling time is", falling_time, "seconds")
