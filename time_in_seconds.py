0b6fcde330ec5d07af04ec27e8deed833002ae7d
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-02-18 13:07:05 +0300

    Time converter has been added in the code

--- time_in_seconds.py
+++ time_in_seconds.py
@@ -1,7 +1,22 @@
-# Total time in seconds calculator
+""" # Total time in seconds calculator
 d = int(input("Enter a number of days "))
 h = int(input("Enter a number of hours "))
 m = int(input("Enter a number of minutes "))
 s = int(input("Enter a number of seconds "))
 res = d * 86_400 + h * 3_600 + m * 60 + s
-print("Total time in seconds is", res)
+print("Total time in seconds is", res) """
+
+# Time converter
+sec_per_d = 86_400
+sec_per_h = 3_600
+sec_per_m = 60
+sec_per_s = 1
+sec = int(input("Enter the total time in seconds "))
+days = sec // sec_per_d
+sec = sec % sec_per_d
+hours = sec // sec_per_h
+sec = sec % sec_per_h
+minutes = sec // sec_per_m
+sec = sec % sec_per_m
+seconds = sec
+print(f"{days} days {hours:02} hours {minutes:02} minutes {seconds:02} seconds")
