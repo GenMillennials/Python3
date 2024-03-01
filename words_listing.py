19963a27eee1c9cf4b3cb3776016e022f244895c
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-01 11:20:23 +0300

    Words listing has been modified

--- words_listing.py
+++ words_listing.py
@@ -1,7 +1,13 @@
-n = int(input())
+""" n = int(input())
 char = 97 + (n - 1)
 ttl = ""
 for i in range(97, char + 1):
     letter = chr(i)
     ttl += letter
-print(list(ttl))
+print(list(ttl)) """
+
+n = int(input())
+s = ""
+for i in range(n):
+    s += chr(ord("a") + i)
+print(list(s))
