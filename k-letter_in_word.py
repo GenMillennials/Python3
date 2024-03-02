c53341f96487366f2ffda8c4f0c6bdf081bc36ab
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-02 20:31:42 +0300

    k-letter in word has been modified

--- k-letter_in_word.py
+++ k-letter_in_word.py
@@ -1,4 +1,4 @@
-n = int(input())
+""" n = int(input())
 L = []
 s = ""
 for _ in range(n):
@@ -7,4 +7,16 @@ k = int(input())
 for i in L:
     if k <= len(i):
         s += i[k - 1]
-print(s)
+print(s) """
+
+n = int(input())
+L = []
+for _ in range(n):
+    L.append(input())
+k = int(input())
+res = ""
+for i in range(n):
+    if len(L[i]) < k:
+        continue
+    res += L[i][k-1]
+print(res)
