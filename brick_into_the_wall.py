b4884a2d840a2f32fbe9d61b1b67f0e2924d6325
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-09 10:16:35 +0300

    Brick into the wall has been modified

--- brick_into_the_wall.py
+++ brick_into_the_wall.py
@@ -1,14 +1,12 @@
 print("Enter length and width the hole")
 a, b = int(input()), int(input())
-a, b = min(a, b), max(a, b)
-print(a,b)
+L = sorted([a, b])
+print(L)
 print("Enter length, width, and high the hole")
 x, y, z = int(input()), int(input()), int(input())
-x, y = min(x, y), max(x, y)
-y, z = min(y, z), max(y, z)
-x, y = min(x, y), max(x, y)
-print(x, y, z)
-if x <= a and y <= b:
+M = sorted([x,y,z])
+print(M)
+if M[0] <= L[0] and M[1] <= L[1]:
     print("Brick will come in the wall.")
 else:
     print("Brick won't come in the wall.")
