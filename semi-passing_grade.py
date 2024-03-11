6f064f79d04c01e10825256cbe9f7e5f76387ede
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-11 15:58:06 +0300

    Passing grade has been modified

--- semi-passing_grade.py
+++ semi-passing_grade.py
@@ -4,5 +4,8 @@ print(f"Enter the points of each applicant ({n})")
 a = list()
 for i in range(n):
     a.append(int(input()))
-print("The semi-passing grade applicants are: ", a.count(a[m]))
+if a[m] != a[m - 1]:
+    print("The semi-passing grade applicants are: ", a.count(a[m]))
+else:
+    print("The semi-passing grade applicants is ", 0)
 
