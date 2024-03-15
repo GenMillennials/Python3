74cfd462b10e9bf506e73adbb35365d771259cb5
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-15 19:20:11 +0300

    Semi-passing grade has been modified

--- semi-passing_grade.py
+++ semi-passing_grade.py
@@ -1,11 +1,20 @@
 n = int(input("Enter the applicants amount: "))
 m = int(input("Enter the college free position: "))
-print(f"Enter the points of each applicant ({n})")
-a = list()
+L = [-1] * m
+cntr = 0
+print("Scores:")
 for i in range(n):
-    a.append(int(input()))
-    a = sorted(a)[::-1]
-    if a[m] == a[m - 1]:
-        print("The semi-passing grade applicants are: ", a.count(a[m]))
-    else:
-        print("The semi-passing grade applicants is ", 0)
+    a = int(input())
+    for j in range(m):
+        if a >= L[j]:
+            L.insert(j, a)
+            if L[-1] == L[-2]:
+                cntr += 1
+            else:
+                cntr = 0
+            L = L[:-1]
+            break
+if cntr != 0:
+    cntr = cntr + L.count(L[-1])
+print("The amount of the semi-passing grade applicants are: ", cntr)
+   
