23dc9b674483805082dc8a6eee15c6c6481f671f
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-23 19:50:36 +0300

    Matrice col has been modified

--- matrice_col.py
+++ matrice_col.py
@@ -3,8 +3,12 @@ M = [[1, 2, 7, 3],
     [4, 5, 6, 6],
     [7, 8, 8, 9]]
 n = len(M)
+maxrow = [max(M[i]) for i in range(n)]
+minrow = [min(M[i]) for i in range(n)]
+maxcol = [max([M[j][i] for j in range(n)]) for i in range(n)]
+mincol = [min([M[j][i] for j in range(n)]) for i in range(n)]
 for i in range(n):
     for j in range(n):
-        if (M[i][j] == max(M[i]) == min([M[k][j] for k in range(n)])
-        or M[i][j] == min(M[i]) == max([M[k][j] for k in range(n)])):
+        if (M[i][j] == maxrow[i] == mincol[j] or
+        M[i][j] == minrow[i] == maxcol[j]):
             print("(", i, j,") = ", M[i][j])
