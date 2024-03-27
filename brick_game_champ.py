9fb8650c500f15e82d7dbb13711ad98b4650cf70
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-27 18:34:07 +0300

    Brick game champ has been modified

--- brick_game_champ.py
+++ brick_game_champ.py
@@ -1,8 +1,23 @@
 n = int(input())
 V = {}
+T = {}
 for i in range(n):
     s = input().split()
     name = s[0]
     score = int(s[1])
     V[name] = score
+    if name not in V or V[name] < score:
+        V[name] = score
+        T[name] = i
 print(V)
+print(T)
+m = max(list(V.values()))
+T1 = {}
+for name in V:
+    if V[name] == m:
+        T1[name] = T[name]
+print(T1)
+t = min(list(T1.values()))
+for name in T1:
+    if T1[name] == t:
+        print(name)
