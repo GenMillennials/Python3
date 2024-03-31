c7fb1cd18c6857c8c42776b4cfe5830a199b5957
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-03-31 12:14:49 +0300

    Password generator has been modified

--- password_generator.py
+++ password_generator.py
@@ -8,20 +8,34 @@ chars = ""
 
 amount = int(input("Enter the general password amount: "))
 length = int(input("Enter the length of a password: "))
+digits_c = input("Is it necessary to include digits in the password? y / n: ").lower()
+lowercase_letters_c = input("Is it necessary to include lowercase letters in the password? y / n: ").lower()
+uppercase_letters_c = input("Is it necessary to include uppercase letters in the password? y / n: ").lower()
+punctuation_c = input("Is it necessary to include punctuation symbols in the password? y / n: ").lower()
 
-chars = digits + lowercase_letters + uppercase_letters + punctuation
+def get_chars():
+    L = []
+    if digits_c == "y" or digits_c == "yes":
+        L.append(digits)
+    if lowercase_letters_c == "y" or lowercase_letters_c == "yes":
+        L.append(lowercase_letters)
+    if uppercase_letters_c == "y" or uppercase_letters_c == "yes":
+        L.append(uppercase_letters)
+    if punctuation_c == "y" or punctuation_c == "yes":
+        L.append(punctuation)
+    return "".join(L)   # str
 
-def generate_single_password():
-    L = list(chars)
+def generate_single_password(get_chars):
+    L = list(get_chars)
     password = str()
     for _ in range(length):
         symbs = random.choice(L)
         password += symbs
-    return password
+    return password   # str()
 
 passwords = list()
 while amount > 0:
-    passwords.append(generate_single_password())
+    passwords.append(generate_single_password(get_chars()))   # list()
     amount -= 1
 
 print(*passwords, sep="\n")
