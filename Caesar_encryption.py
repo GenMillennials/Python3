bf899d48a3e715c97d2a3016251349729d294605
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-03 09:24:55 +0300

    Caesar encryption has been modified

--- Caesar_encryption.py
+++ Caesar_encryption.py
@@ -1,23 +1,35 @@
 print("Welcome to Caesar's encryption and decryption generator!")
 action = input("Would you like to encrypt of decript your data? encr / decr").lower()
-if action == "encr" or action == "enc" or action == "en" or action == "e":
-    pass
-elif action == "decr" or action == "dec" or action == "de" or action == "d":
-    pass
 lang = input("Pick the language: eng / rus").lower()
 eng_lang = "abcdefghijklnopqrstuvwxyz"
 rus_lang = "абвгдежзиклмнопрстуфхцчшщъыьэюя"
 rotate = int(input("Pick the rotate index: "))
 print("Enter your text for encryption or decryption:")
 data = input()
-print(data)
 
-def encryption(data):
-    L = [data]
-    if lang == "eng" or lang == "e" or lang == "english":
-        pass
-    if lang == "rus" or lang == "r" or lang == "russian":
-        pass
+def pick_action_lang():
+    global action
+    if action == "encr" or action == "enc" or action == "en" or action == "e":
+        action = is_encryption
+    elif action == "decr" or action == "dec" or action == "de" or action == "d":
+        action = is_decryption
+    return action   # 
 
-def decryption(data):
+def pick_lang():
+    global eng_lang, rus_lang
+    if lang == "eng" or lang == "e" or lang == "en" or lang == "english":
+        eng_lang = lang
+    if lang == "rus" or lang == "r" or lang == "ru" or lang == "russian":
+        rus_lang = lang
+    return lang   # str()
+
+def is_encryption(data, rotate):
+    L = list(data)
+    for i in L:
+        if i.isupper():
+            s = # range(65, 91)
+        else:
+            s = chr(ord(i) + rotate)
+
+def is_decryption(data, rotate):
     pass
