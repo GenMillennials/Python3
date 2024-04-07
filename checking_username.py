96960558297192bddd32cee211db2e3c976edd2e
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-07 20:17:46 +0300

    Checking username has been modified

--- checking_username.py
+++ checking_username.py
@@ -5,10 +5,6 @@ if username[0] != "@":
 if len(username) < 5 or len(username) > 15:
     flag2 = False
 for i in username[1::]:
-    if i == i.isalnum():
-        flag3 = True
-if username[1::].islower():
-    flag3 = True
-else:
-    flag3 = False
+    if not (i.isalnum() and (i.islower() or i.isdigit())):
+        flag3 = False
 print("Correct" if flag1 == True and flag2 == True and flag3 == True else "Incorrect")
