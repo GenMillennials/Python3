f003686b54077bcf86f5caf71ef3142d4db46938
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-09 21:44:21 +0300

    Comments analysis has been modified

--- comments_analysis.py
+++ comments_analysis.py
@@ -1,6 +1,6 @@
-for i in range(1, int(input("Enter string amount")) + 1):
+for i in range(1, int(input("Enter string amount: ")) + 1):
     cmnt = input()
-    if cmnt == " " or cmnt.isspace():
+    if not (cmnt == "" or cmnt.isspace()):
         print(f"{i}: {cmnt}")
     else:
-        print(f"{i}: COMMENT NOT FOUND")
+        print(f"{i}: COMMENT SHOULD BE DELETED")
