7e7a5806fef985ba193340e88faadbd3e8f927c5
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-16 10:18:30 +0300

    Caesar encryption has been modified

--- Caesar_encryption.py
+++ Caesar_encryption.py
@@ -1,26 +1,8 @@
 def welcome():
     print("Welcome to Caesar's encryption and decryption generator!")
 
-def is_action():
-    action = input("Would you like to encrypt of decript your data? encr / decr ").lower()
-    # if action == "encr" or action == "enc" or action == "en" or action == "e" or action == "encryption":
-    #     is_encryption = action
-    # elif action == "decr" or action == "dec" or action == "de" or action == "d" or action == "decryption":
-    #     is_decryption = action
-    return action
-
-def pick_lang():
-    eng_lang = "abcdefghijklnopqrstuvwxyz"
-    rus_lang = "абвгдежзиклмнопрстуфхцчшщъыьэюя"
-    lang = input("Pick the language: eng / rus ").lower()
-    if lang == "eng" or lang == "e" or lang == "en" or lang == "english":
-        lang = eng_lang
-    if lang == "rus" or lang == "r" or lang == "ru" or lang == "russian":
-        lang = rus_lang
-    return lang   # str()
-
 def enter_data():
-    print("Enter your text for encryption or decryption:")
+    print("Enter your text:")
     data = input()
     return data   # str()
 
@@ -53,6 +35,9 @@ def is_encryption_rus(data, rotate):
         if s.islower():
             s = chr((((ord(i) - 1072) + rotate) % 32) + 1072)
             L.append(s)
+        else:
+            s = i
+            L.append(s)
     return "".join(L)   # str()
 
 def is_decryption_eng(data, rotate):
@@ -64,6 +49,9 @@ def is_decryption_eng(data, rotate):
             L.append(s)
         elif i.islower():
             s = chr((((ord(i) - 97) - rotate) % 26) + 97)
+            L.append(s)
+        else:
+            s = i
             L.append(s)
     return "".join(L)   # str()
 
@@ -77,10 +65,19 @@ def is_decryption_rus(data, rotate):
         elif i.islower():
             s = chr((((ord(i) - 1072) - rotate) % 32) + 1072)
             L.append(s)
+        else:
+            s = i
+            L.append(s)
     return "".join(L)   # str()
 
-#welcome()
-#is_action()
-#pick_lang()
-#enter_data()
-#rotate()
+welcome()
+action = input("Would you like to encrypt of decript your data? encr / decr ").lower()
+lang = input("Pick the language: eng / rus ").lower()
+if (lang == "eng" or lang == "e" or lang == "en" or lang == "english") and (action == "encr" or action == "e" or action == "enc" or action == "en" or action == "encryption"):
+    print(is_encryption_eng(enter_data(), rotate()))
+elif (lang == "rus" or lang == "r" or lang == "ru" or lang == "russian") and (action == "encr" or action == "e" or action == "enc" or action == "en" or action == "encryption"):
+    print(is_encryption_rus(enter_data(), rotate()))
+elif (lang == "eng" or lang == "e" or lang == "en" or lang == "english") and (action == "decr" or action == "de" or action == "dec" or action == "de" or action == "d" or action == "decryption"):
+    print(is_decryption_eng(enter_data(),rotate()))
+elif (lang == "rus" or lang == "r" or lang == "ru" or lang == "russian") and (action == "decr" or action == "de" or action == "dec" or action == "de" or action == "d" or action == "decryption"):
+    print(is_decryption_rus(enter_data(),rotate()))
