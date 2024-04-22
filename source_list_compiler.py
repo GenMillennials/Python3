48514671f6dc9a0f0edbf7a78f8ed4b192bb10ff
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-22 11:33:11 +0300

    Source list compiler has been modified

--- source_list_compiler.py
+++ source_list_compiler.py
@@ -1,12 +1,34 @@
-def get_law(annot):
-    res = ""
-    url = ""
-    comp = annot.split()
-    for i in comp:
-        if i == "https://www.consultant":
-            res = i + " " + "// СПС \"Консультант Плюс\" - URL:" + url
-        elif i == "https://www.garant":
-            res =
+def get_authors():
+    print("Введите всех авторов через запятую:")
+    authors = input()
+    return authors
+
+def get_material():
+    print("Введите название материала:")
+    src_material = input()
+    return src_material
+
+def get_name_src():
+    print("Введите название источника:")
+    name_src = input()
+    return name_src
+
+def get_year():
+    print("Введите год публикации:")
+    src_year = input()
+    return src_year
+
+def src_number():
+    print("Введите номер периодического издания:")
+    src_num = input()
+    return src_num
+
+def src_strings():
+    print("Введите страницу или страницы:")
+    src_strings = input()
+    return src_strings
+
+def get_law():  
 
 # def get_websource(annot):
 
@@ -17,8 +39,3 @@ def get_law(annot):
 # def get_confer(annot):
 
 # def get_textbook(annot):
-
-
-# print("Введите текст источника:")
-# source = input()
-print(get_law("Абвг, дежз иклмн."))
