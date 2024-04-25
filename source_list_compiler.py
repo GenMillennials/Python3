88dbcac7ffec9fa35723e7d8be431103ce867f8e
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-04-25 14:17:39 +0300

    Source list compiler has been modified

--- source_list_compiler.py
+++ source_list_compiler.py
@@ -1,12 +1,10 @@
-def pick_source():
-    print("Выберите вид источника публикации")
+def pick_source(type):
     while True:
-        pick_type_src = input("Если журнал - ж, диссертация - д,\nавтореферат - a, конференция - к, НПА - нпа\n")
-        if pick_type_src in ("ж", "д", "а", "к", "нпа"):
+        if type in ("ж", "д", "а", "к", "нпа"):
             break
         else:
-            print("Введите корректное значение из предложенных")
-    return pick_type_src
+            type = input("Попробуйте еще раз: ")
+    return type
 
 def get_name_src():
     name_src = input("Введите название источника:\n")
@@ -36,14 +34,17 @@ def get_year():
 
 def get_volume():
     while True:
-        src_volume = input("Введите том книги или журнала:\n")
+        src_volume = input("Введите том книги или журнала:\nЕсли том отсутствует, введите ноль: ")
         if src_volume.isdigit() and len(src_volume) <= 2 and src_volume[0] != "0":
+            break
+        elif src_volume == "0":
+            src_volume = ""
             break
         else:
             print("Номер тома издания некорректен")
     return src_volume
 
-def src_number():
+def get_src_number():
     while True:
         src_num = input("Введите номер периодического издания:\n")
         if src_num.isdigit() and len(src_num) <= 3:
@@ -52,29 +53,20 @@ def src_number():
             print("Введенный номер издания некорректен")
     return src_num
 
-def src_strings():
+def get_src_page():
     while True:
         src_strings = input("Введите страницу или страницы:\n")
-        if src_strings.isdigit() and len(src_strings) <= 3:
+        if src_strings != src_strings.isalpha() and len(src_strings) <= 7:
             break
         else:
             print("Номера страниц указаны некорректно")
     return src_strings
 
-# def get_law():
-
-# def get_websource(annot):
-
-# def get_monography(annot):
-
-# def get_article(annot):
-
-# def get_confer(annot):
-
-# def get_textbook(annot):
 print("Давайте начнем описание источника")
-# pick_source()
-print(src_strings())
-
-
-
+res = ""
+print("Выберите вид источника публикации")
+pick_type_src = input("Если журнал - ж, диссертация - д,\nавтореферат - a, конференция - к, НПА - нпа\n")
+if pick_source(pick_type_src) == "ж" and get_volume != "0":
+    res = get_authors().title() + " " + get_name_src().capitalize() + " // " + get_material().title() + ". " + get_year() + ". " + get_volume() + ". № " + get_src_number() + ". " + "С." + get_src_page() + "."
+elif pick_source(pick_type_src) == "ж" and get_volume == "0":
+    res = get_authors().title() + " " + get_name_src().capitalize() + " // " + get_material().title() + ". " + get_year() + "№ " + get_src_number() + ". " + "С. " + get_src_page() + "."
