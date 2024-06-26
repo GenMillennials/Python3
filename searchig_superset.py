e61e12a2aa1e19a66b814c51c70eba97332cf1dc
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-06-26 13:35:54 +0300

    Searching superset in has been created

---
+++ searchig_superset.py
@@ -0,0 +1,7 @@
+# The searching superset
+
+n1, n2 = set(input()), set(input())   # Creation two sets
+if n1 >= n2:   # Searching the supperset or .issuperset() method
+    print("YES")
+else:
+    print("NO")
