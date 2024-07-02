c148f4eda61b4ae2e2bd6e7efa2241cef9e65582
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-07-02 16:30:26 +0300

    The first letter in the set output has been created

---
+++ set_with_first_letter_every_word.py
@@ -0,0 +1,5 @@
+# The first letter in the set output
+
+words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes', 'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+set1 = {i[0].lower() for i in words}
+print(*sorted(set1))
