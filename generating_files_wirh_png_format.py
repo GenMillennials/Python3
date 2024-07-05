e2b0b2bc3e8c712870b037a36f538ac0ff9bf0f4
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-07-05 07:49:08 +0300

    Generating files with png format has been created

---
+++ generating_files_wirh_png_format.py
@@ -0,0 +1,6 @@
+# Generating files with .png format
+
+files = ['python.png', 'qwerty.py', 'stepik.png', 'beegeek.org', 'windows.pnp', 'pen.txt', 'phone.py', 'book.txT', 'board.pNg', 'keyBoard.jpg', 'Python.PNg', 'apple.jpeg', 'png.png', 'input.tXt', 'split.pop', 'solution.Py', 'stepik.org', 'kotlin.ko', 'github.git']
+
+set1 = {i.lower() for i in files if i[-3:].lower() == "png"}
+print(*sorted(set1))
