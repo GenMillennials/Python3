3e94c09402ed7c419aee0eb92ad7503e0273e709
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-07-06 17:54:28 +0300

    Function days in month has been created

---
+++ day_in_month_function.py
@@ -0,0 +1,14 @@
+# объявление функции
+def get_days(month):
+    if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        return 31
+    elif month == 4 or month == 6 or month == 9 or month == 11:
+        return 30
+    else:
+        return 28
+
+# считываем данные
+num = int(input())
+
+# вызываем функцию
+print(get_days(num))
