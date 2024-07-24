e277b2f13d77e45210063c97a241162861eb74e3
Bondarenko Yu.A. <bondarenko_yuri@bk.ru>
2024-07-24 08:14:54 +0300

    Counting amount the letters in string with dict has been created

---
+++ counting_symb_in_dict.py
@@ -0,0 +1,7 @@
+text = 'footballcyberpunkextraterritorialityconversationalistblockophthalmoscopicinterdependencemamauserfff'
+
+result = {}
+
+for key in text:
+    result[key] = result.get(key, 0) + 1
+print(result)
